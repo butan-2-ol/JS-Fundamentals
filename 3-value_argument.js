6522ab4e1f14398f3612842a40e23e18ced1ad54
butan-2-ol <champamartey01@gmail.com>
2025-11-09 17:27:36 +0000

    initial comit if javascript fundamentals

---
+++ 3-value_argument.js
@@ -0,0 +1,9 @@
+// get the first argument
+const firstArg = process.argv[2];
+
+// check if the argument exists
+if (firstArg === undefined) {
+  console.log("No argument");
+} else {
+  console.log(firstArg);
+}
